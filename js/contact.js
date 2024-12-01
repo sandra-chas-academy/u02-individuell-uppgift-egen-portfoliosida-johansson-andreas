65e8ca4b37e1ba5cb3e903039e9a403a7c12188c
Andreas Johansson <andreas.johansson@chasacademy.se>
2024-12-01 12:47:57 +0100

    added hamburglar puzzle onb contact page

--- js/contact.js
+++ js/contact.js
@@ -1,10 +1,51 @@
 export const renderContactsPage = () => {
-    return `
+  const puzzleHTML = generatePuzzle();
+
+  return `
      <div class="contact">
+        ${puzzleHTML}
         <h1 class="contacts__title"></h1>
         <h2 class="gradient_text">hamburglar@mcdonalds.com</h2>
         <br><span class="contacts__gray-text">or</span><br>
         <h2 class="gradient_text">+23 043 210 60</h2>
 
-    </div>`
+    </div>`;
+};
+
+const generatePuzzle = () => {
+  let arr = [1, 2, 3, 4];
+
+  const shuffledArr = shuffleArray(arr);
+
+  let piecesHTML = "";
+  shuffledArr.forEach((index) => {
+    piecesHTML += `<div id="puzzle-image-${index}" class="puzzle-image-${index} puzzle__puzzle-piece"></div>`;
+  });
+
+  let puzzleHTML = `<div class="puzzle"> ${piecesHTML} </div>`;
+
+  return puzzleHTML;
+};
+
+const shuffleArray = (arr) => {
+    const originalArray = [...arr];  
+    let shuffledArray;
+  
+    do {
+      shuffledArray = [...arr];  
+      for (let i = shuffledArray.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));  
+        [shuffledArray[i], shuffledArray[j]] = [shuffledArray[j], shuffledArray[i]]; 
+      }
+    } while (arraysAreEqual(originalArray, shuffledArray)); 
+  
+    return shuffledArray;
+  };
+  
+const arraysAreEqual = (arr1, arr2) => {
+if (arr1.length !== arr2.length) return false;
+for (let i = 0; i < arr1.length; i++) {
+    if (arr1[i] !== arr2[i]) return false;
 }
+return true;
+};
--- js/main.js
+++ js/main.js
@@ -34,6 +34,7 @@ const renderHeader = () => {
 };
 
 const transitionToPage = async (pageName) => {
+  document.body.style.overflow = "hidden";
   const currentContent = document.querySelector("main > div");
 
   const oldIndex = Object.keys(router).indexOf(currentContent.classList[0]);
@@ -60,6 +61,8 @@ const transitionToPage = async (pageName) => {
   }
 
   setTimeout(() => {
+    document.body.style.overflow = "unset";
+
     transitioning = false;
     pageMain.removeChild(newContent);
     currentContent.innerHTML = newContent.innerHTML;
@@ -85,18 +88,124 @@ const renderFooter = async () => {
   pageFooter.innerHTML = footerContent(); 
 
 }
+let lastMousePos;
+let squares;
+let zIndexes = [];
 
-document.body.addEventListener("mouseover", (e) => {
-  console.log("mouseover")
-  const card = e.target.closest(".projects__card");
-  if (card && !card.contains(e.relatedTarget)) {
-    console.log("Trigger")
-    const blur = document.createElement("div");
-    blur.classList.add("blur");
-    document.body.appendChild(blur);
+document.body.addEventListener("mousedown", (e) => {
+  if(e.target.classList.contains("puzzle__puzzle-piece")) {
+    squares = document.querySelectorAll(".puzzle__puzzle-piece");
+    if(zIndexes.includes(e.target)) zIndexes.splice(zIndexes.indexOf(e.target), 1)
+    zIndexes.push(e.target)
+    zIndexes.forEach((square, index) => {
+      square.style.zIndex = index + 1;
+    })
+    // const startCoords = [e.target.offsetLeft, e.target.offsetTop];
+    lastMousePos = { x: e.clientX, y: e.clientY };
+    e.target.addEventListener("mousemove", moveSquare)
   }
-});
+})
+document.body.addEventListener("mouseup", (e) => {
+  squares = document.querySelectorAll(".puzzle__puzzle-piece");
 
+  let arrayToSort = [...squares];
+  arrayToSort = sortByXPos(arrayToSort);
+
+  if(arrayToSort.length == 0) return
+
+  let squarePositions = {};
+
+  if(arrayToSort[0].offsetLeft < arrayToSort[1].offsetLeft) {
+    squarePositions.topLeft = arrayToSort[0];
+    squarePositions.topRight = arrayToSort[1];
+  }
+  else {
+    squarePositions.topLeft = arrayToSort[1];
+    squarePositions.topRight = arrayToSort[0]
+  }
+
+  if(arrayToSort[2].offsetLeft < arrayToSort[3].offsetLeft) {
+    squarePositions.bottomLeft = arrayToSort[2];
+    squarePositions.bottomRight = arrayToSort[3];
+  }
+  else {
+    squarePositions.bottomLeft = arrayToSort[3];
+    squarePositions.bottomRight = arrayToSort[2]
+  }
+
+
+  if(checkPosition(squarePositions)) {
+    if(squarePositions.topLeft.id == "puzzle-image-1" && squarePositions.topRight.id == "puzzle-image-2" && squarePositions.bottomLeft.id == "puzzle-image-3" && squarePositions.bottomRight.id == "puzzle-image-4") {
+      const puzzle = document.body.querySelector(".puzzle")
+
+      if(document.body.querySelector(".winner-div")) {
+      }
+      else {
+        const winText = document.createElement("div")
+        winText.classList.add("winner-div")
+        winText.innerHTML = "Yay you are win. Much win. Many win"
+  
+        puzzle.parentNode.insertBefore(winText, puzzle.nextSibling);
+      }
+    }
+  }
+
+  squares.forEach(square => {
+    square.removeEventListener("mousemove", moveSquare)
+  })
+})
+
+const sortByXPos = (array) => {
+  const sortedArray = array.sort((a, b) => a.offsetTop - b.offsetTop)
+
+  return sortedArray;
+}
+
+const checkPosition = (squares) => {
+  const parent = squares.topLeft.parentElement;
+
+  const parentRect = parent.getBoundingClientRect();
+
+  const isInsideParent = (x, y) => {
+    return (
+      x >= parentRect.left && x <= parentRect.right &&
+      y >= parentRect.top && y <= parentRect.bottom
+    );
+  };
+
+  const topLeft = squares.topLeft.getBoundingClientRect();
+  const topLeftInside = isInsideParent(topLeft.left, topLeft.top);
+
+  const topRight = squares.topRight.getBoundingClientRect();
+  const topRightInside = isInsideParent(topRight.right, topRight.top);
+
+  const bottomLeft = squares.bottomLeft.getBoundingClientRect();
+  const bottomLeftInside = isInsideParent(bottomLeft.left, bottomLeft.bottom);
+
+  const bottomRight = squares.bottomRight.getBoundingClientRect();
+  const bottomRightInside = isInsideParent(bottomRight.right, bottomRight.bottom);
+
+  if (topLeftInside && topRightInside && bottomLeftInside && bottomRightInside) {
+    return true;
+  } else {
+    return false;
+  }
+};
+
+const moveSquare = (event) => {
+  if (lastMousePos) {
+    const deltaX = event.clientX - lastMousePos.x; 
+    const deltaY = event.clientY - lastMousePos.y; 
+
+    const currentX = parseInt(event.target.style.left || 0, 10);
+    const currentY = parseInt(event.target.style.top || 0, 10);
+
+    event.target.style.left = `${currentX + deltaX}px`;
+    event.target.style.top = `${currentY + deltaY}px`;
+
+    lastMousePos = { x: event.clientX, y: event.clientY };
+  }
+}
 
 renderHeader();
 renderContent();
